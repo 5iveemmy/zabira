7c9360686fa49c14fd8b469243da32148ac27cde
5iveemmy <ainemmanuel87@gmail.com>
2024-11-19 22:24:51 +0100

    chore: implement footer design

---
+++ components/footer.tsx
@@ -0,0 +1,284 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { Separator } from "./ui/separator";
+import Link from "next/link";
+import Image from "next/image";
+import {
+  BiLogoFacebookCircle,
+  BiLogoInstagram,
+  BiLogoLinkedinSquare,
+  BiLogoTwitter,
+  BiLogoYoutube,
+} from "react-icons/bi";
+import Icon from "./Icon";
+import { IoIosArrowUp } from "react-icons/io";
+import { Button } from "./ui/button";
+
+interface ListDisplayProps {
+  title: string;
+  items: { text: string; link: string }[];
+}
+
+const ListDisplay = ({ title, items }: ListDisplayProps) => {
+  return (
+    <div className="flex items-center md:items-start gap-2 flex-col col-span-1">
+      <h2 className="font-bold text-sm pb-2">{title}</h2>
+      {items.map(({ text, link }) => (
+        <div key={text} className="flex text-center items-center gap-2">
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-brand text-lg "
+          >
+            {text}
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const listData = [
+  {
+    title: "COMPANY",
+    items: [
+      { text: "About Us", link: "" },
+      { text: "Contact US", link: "" },
+      { text: "Career", link: "" },
+    ],
+  },
+  {
+    title: "PRODUCTS",
+    items: [
+      { text: "Trade Crypto", link: "" },
+      {
+        text: "Trade Giftcards",
+        link: "",
+      },
+      { text: "Utility Bills", link: "" },
+    ],
+  },
+  {
+    title: "SUPPORT",
+    items: [
+      { text: "Blog", link: "" },
+      { text: "Download", link: "" },
+      { text: "Help Center", link: "" },
+      { text: "Referral Program", link: "" },
+      { text: "ID Verification", link: "" },
+    ],
+  },
+  {
+    title: "LEGAL",
+    items: [
+      { text: "Terms Of Use", link: "" },
+      { text: "Privacy Policy", link: "/privacy/" },
+    ],
+  },
+];
+
+const socialLinks = [
+  {
+    icon: <BiLogoFacebookCircle />,
+    link: "https://web.facebook.com/thezabira/",
+  },
+  {
+    icon: <BiLogoInstagram />,
+    link: "https://www.instagram.com/thezabira/",
+  },
+  {
+    icon: <BiLogoYoutube />,
+    link: "https://www.youtube.com/embed/LyNADeA9dl8&rel=0showinfo=0",
+  },
+  {
+    icon: <BiLogoTwitter />,
+    link: "https://x.com/TheZabira",
+  },
+  {
+    icon: <BiLogoLinkedinSquare />,
+    link: "https://www.linkedin.com/company/thezabira/posts/",
+  },
+];
+
+const Footer = () => {
+  const [showScrollTop, setShowScrollTop] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setShowScrollTop(window.scrollY > 300);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <>
+      <section className="bg-brand mt-[70px] pt-14 zabira-download relative">
+        <div className="pt-24 custom-container flex items-center gap-96">
+          <div className="lg:w-[35%] flex flex-col gap-1 text-white">
+            <h1 className="text-5xl leading-tight  font-bold">
+              Download the Zabira App Today!
+            </h1>
+            <p className="text-lg">
+              We&lsquo;ve done it carefully and simply. Combined with the
+              ingredients makes for beautiful landings
+            </p>
+
+            <div className="gap-4 flex py-4">
+              <Image
+                src="/app-store-badge.svg"
+                width="151"
+                height="41"
+                className="rounded-2xl"
+                alt="app store badge"
+              />
+              <Image
+                src="/google-play-badge.svg"
+                width="151"
+                className="rounded-2xl"
+                height="41"
+                alt="google play badge"
+              />
+            </div>
+          </div>
+          <div>
+            <Image
+              priority
+              width="317"
+              height="572"
+              src="/phone.svg"
+              alt="phone"
+            />
+          </div>
+        </div>
+        <Image
+          priority
+          width="192"
+          height="109"
+          src="/three-arrows.svg"
+          alt="arrows"
+          className="absolute top-[5rem] right-[8rem]"
+        />
+
+        <Image
+          src="/plane.svg"
+          width="160"
+          height="86"
+          alt="plane icon"
+          className="absolute bottom-[5.5rem] right-[15.5rem]"
+        />
+      </section>
+
+      <section className="promo-section">
+        <div className="py-8 flex justify-between custom-container items-center">
+          <div className="text-white">
+            <p className="font-bold text-[32px] pb-2">
+              Get Up to $30 USDT Free!
+            </p>
+            <p>
+              Monthly Top Trader Wins $30 USDT: Be the champion trader to claim
+              your prize!
+            </p>
+          </div>
+
+          <Button
+            asChild
+            className="bg-white h-12 text-dark rounded-full py-3 px-8 text-base font-bold transition-transform duration-300 ease-in-out hover:bg-dark hover:text-white hover:scale-105"
+          >
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://app.zabira.com/sign-up"
+            >
+              Get Started
+            </a>
+          </Button>
+        </div>
+      </section>
+      <div className="custom-container">
+        <div className="flex">
+          <div className="border-r border-[#8c98a4] pr-32 pt-11 min-w-[400px]">
+            <Link href="/">
+              <Image
+                priority
+                width="189"
+                height="52"
+                src="/logo.svg"
+                className="mb-6"
+                alt="zabira logo"
+              />
+            </Link>
+            <p className="text-[#7e7e7e]">+234 01 887 0002</p>
+            <p className="text-[#7e7e7e]">support@zabira.com</p>
+          </div>
+
+          <div className="border-r pb-8 border-[#8c98a4] pl-20 pr-12 pt-11 flex flex-wrap max-w-[471px] justify-between gap-10">
+            {listData.map(({ title, items }) => (
+              <ListDisplay key={title} title={title} items={items} />
+            ))}
+          </div>
+
+          <div className="flex flex-col items-center pt-[53px] pl-28 pb-16">
+            <p className="text-3xl mb-3 font-bold">Download our app </p>
+            <Image
+              priority
+              width="150"
+              height="150"
+              src="/qrcode_2.png"
+              className="mb-6"
+              alt="zabira logo"
+            />
+            <div className="flex gap-4 items-center ">
+              {socialLinks.map(({ icon, link }, index) => (
+                <Icon
+                  key={index}
+                  icon={icon}
+                  link={link}
+                  className="text-3xl hover:text-brand"
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <Separator className="opacity-80 " />
+
+        <p className="text-[#7e7e7e]  py-4 text-center">
+          Copyright © 2024. All rights reserved.{" "}
+          <a className="hover:text-brand font-bold text-dark cursor-pointer">
+            Terms Of Service
+          </a>{" "}
+          |{" "}
+          <a className="hover:text-brand font-bold text-dark cursor-pointer">
+            Privacy Policy
+          </a>
+        </p>
+      </div>
+      <div className="fixed bottom-6 right-6">
+        {showScrollTop && (
+          <button
+            onClick={scrollToTop}
+            className="bg-[#53668b1a] flex items-center justify-center p-3 rounded-lg shadow-md hover:bg-brand/20 transition-all"
+            aria-label="Scroll to top"
+          >
+            <IoIosArrowUp className="text-lg text-[#404e6b] opacity-50" />
+          </button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Footer;
