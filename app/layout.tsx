a2d247aebdb3a550c30422528637bb18a1178f03
5iveemmy <ainemmanuel87@gmail.com>
2024-11-19 17:36:51 +0100

    chore: add navbar and footer to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "./themeProvider";
+import dynamic from "next/dynamic";
+
+const Navbar = dynamic(() => import("@/components/navbar"), { ssr: false });
+const Footer = dynamic(() => import("@/components/footer"), { ssr: false });
 
 const plusJakarta = localFont({
   src: "./fonts/PlusJakartaSans-VariableFont_wght.ttf",
@@ -25,7 +29,11 @@ export default function RootLayout({
         className={`${plusJakarta.variable} ${plusJakarta.variable} antialiased`}
       >
         <ThemeProvider attribute="class" enableSystem disableTransitionOnChange>
-          <main className="plus_jakarta_sans">{children}</main>
+          <main className="plus_jakarta_sans">
+            <Navbar />
+            {children}
+            <Footer />
+          </main>
         </ThemeProvider>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,28 @@
 
 import dynamic from "next/dynamic";
 
-const Navbar = dynamic(() => import("@/components/navbar"), { ssr: false });
-const HomePage = dynamic(() => import("./containers/home-page"), {
+const HeroSection = dynamic(
+  () => import("./containers/home-page/hero-section"),
+  {
+    ssr: false,
+  }
+);
+const OverviewSection = dynamic(
+  () => import("./containers/home-page/overview-section"),
+  {
+    ssr: false,
+  }
+);
+const WhatWeDo = dynamic(() => import("./containers/home-page/what-we-do"), {
   ssr: false,
 });
 
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <HomePage />
+      <HeroSection />
+      <OverviewSection />
+      <WhatWeDo />
     </>
   );
 }
