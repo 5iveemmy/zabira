7592c0ebe08be0de7d70fee29f64ab7480bda715
5iveemmy <ainemmanuel87@gmail.com>
2024-11-23 16:16:22 +0100

    chore: create statsWithImage component

--- app/company/about/page.tsx
+++ app/company/about/page.tsx
@@ -1,73 +1,77 @@
 "use client";
 
-import dynamic from "next/dynamic";
-import Image from "next/image";
+import StatsWithImage from "@/components/statsWithImage";
+import ZabiraFootprint from "@/components/zabira-footprint";
 import React from "react";
 
-const Header = dynamic(() => import("@/components/zabira-footprint"), {
-  ssr: false,
-});
+const infoData = [
+  {
+    title: "Who We Are",
+    description: [
+      "The Grand Masters of Instant Payments. Zabira is your trusted Partner in digital finance, specializing in instant payments. We offer a state-of-the-art pro platform for cryptocurrency trading, gift card exchanges, utility bill payments, and fiat transactions. Our innovative solutions simplify the way you manage your money, empowering you to manage the digital economy with ease.",
+      "The Zabira Wallet is a secure and convenient way to manage your money.",
+    ],
+    infoHighlight: [
+      {
+        header: "Mission",
+        description:
+          "To promote payment solutions at the most competitive rates and facilitate quick settlement of money transfer.",
+        icon: "/about-mission.svg",
+      },
+      {
+        header: "Vision",
+        description:
+          "To be the customer’s first choice of crypto trading and fast payment solutions in Africa and beyond.",
+        icon: "/about-vision.svg",
+      },
+      {
+        header: "Core Values",
+        description: `The Zabira brand values spans across team members and end-users, are strongly centered on people. This emphasis is reflected in our B.E.S.C.T.T acronym where we have  "Business Ethnics, Excellence, Social responsibility, Customer Service, Trustworthy, Team work`,
+        icon: "/about-core-values.svg",
+      },
+    ],
+  },
+  {
+    title: "Safe and secure way to finance",
+    description: [
+      "The Zabira wallet provides a secure and convenient money management solution.",
+    ],
+    infoHighlight: [
+      {
+        header: "24hours support",
+        description:
+          "Our support team are available around the clock to ensure your financial security by providing assistance whenever you need it.",
+        icon: "/24hrs-support.svg",
+      },
+      {
+        header: "Security",
+        description:
+          "At Zabira, we take the safety of your finance seriously by using industry-standard encryption, fraud prevention tools, and secure login processes.",
+        icon: "/security.svg",
+      },
+      {
+        header: "Payment integrity",
+        description: `Secure payment gateway,protection of funds with rigorous security measures and safe transactions.`,
+        icon: "/payment-integrity.svg",
+      },
+    ],
+  },
+];
 
 const About = () => {
   return (
     <>
-      <Header
+      <ZabiraFootprint
         title="About"
         blueText
         description="The grand masters of instant payments"
       />
 
       <section>
-        <div className="custom-container py-5  flex justify-between">
-          <Image
-            priority
-            width="558"
-            className="px-4"
-            height="528"
-            src="/about.png"
-            alt="about"
-          />
-
-          <div className="pl-4  pr-44">
-            <h3 className="mb-3 text-5xl font-bold">Who We Are</h3>
-            <div>
-              <p className="mb-3 text-xl  text-[#7e7e7e]">
-                The Grand Masters of Instant Payments. Zabira is your trusted
-                Partner in digital finance, specializing in instant payments. We
-                offer a state-of-the-art pro platform for cryptocurrency
-                trading, gift card exchanges, utility bill payments, and fiat
-                transactions. Our innovative solutions simplify the way you
-                manage your money, empowering you to manage the digital economy
-                with ease.
-              </p>
-              <p className="text-xl  text-[#7e7e7e] ">
-                The Zabira Wallet is a secure and convenient way to manage your
-                money.
-              </p>
-            </div>
-            <div className="flex flex-col gap-7 mt-11 pr-24">
-              <div className="flex gap-8 items-center">
-                <div>
-                  <div className="bg-[#3772ff1a] rounded-full  w-[88px] h-[88px] flex items-center justify-center">
-                    <Image
-                      priority
-                      width="100"
-                      height="100"
-                      src="/about-mission.svg"
-                      alt="mission"
-                    />
-                  </div>
-                </div>
-                <div>
-                  <p className="mb-3 text-2xl font-bold">Mission</p>
-                  <p className="text-[#7e7e7e]">
-                    To promote payment solutions at the most competitive rates
-                    and facilitate quick settlement of money transfer.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className="custom-container flex flex-col gap-16">
+          {infoData.map((data, index) => (
+            <StatsWithImage index={index} key={index} infoData={data} />
+          ))}
         </div>
       </section>
 
---
+++ components/statsWithImage.tsx
@@ -0,0 +1,116 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import React from "react";
+
+const tradersData = [
+  {
+    icon: "/global.svg",
+    value: "3",
+    title: "Countries",
+    className: "top-14 left-[3.5rem]",
+  },
+  {
+    icon: "/reload.svg",
+    value: "$350k+",
+    title: "Daily transactions",
+    className: "top-[55%] right-2 transform  -translate-y-1/2",
+  },
+  {
+    icon: "/avatarPlus.svg",
+    value: "10K+",
+    title: "Trades",
+    className: "top-[73%] left-0 transform  -translate-y-1/2 pr-6",
+  },
+];
+
+interface Props {
+  index: number;
+  infoData: {
+    title: string;
+    description: string[];
+    infoHighlight: {
+      header: string;
+      description: string;
+      icon: string;
+    }[];
+  };
+}
+
+const StatsWithImage = ({ index, infoData }: Props) => {
+  return (
+    <div
+      className={`${
+        index % 2 !== 0 ? "flex-row-reverse" : "flex-row"
+      } py-5 w-full flex justify-between gap-44`}
+    >
+      <div className="relative h-fit">
+        <Image
+          priority
+          height="558"
+          width="528"
+          src="/Illustration.png"
+          alt="about"
+          className="max-w-fit"
+        />{" "}
+        {tradersData.map(({ title, value, icon, className }) => (
+          <div
+            key={title}
+            style={{ boxShadow: "0px 14px 64px 0px rgba(15, 15, 15, 0.1)" }}
+            className={cn(
+              "bg-white py-5 w-fit px-16 absolute flex rounded-xl justify-between gap-16",
+              className
+            )}
+          >
+            <Image
+              priority
+              width="26"
+              height="26"
+              src={icon}
+              alt="global icon"
+            />
+            <div>
+              <p className="text-2xl font-bold">{value}</p>
+              <p className="text-lg text-[#7e7e7e] text-nowrap">{title}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="pl-4 ">
+        <h3 className="mb-3 text-5xl font-bold">{infoData.title}</h3>
+        <div className="flex flex-col gap-3">
+          {infoData.description.map((desc) => (
+            <p className="text-xl  text-[#7e7e7e]" key={desc}>
+              {desc}
+            </p>
+          ))}
+        </div>
+
+        <div className="flex flex-col gap-7 mt-11 pr-24">
+          {infoData.infoHighlight.map(({ header, description, icon }) => (
+            <div key={header} className="flex gap-8 items-center">
+              <div>
+                <div className="bg-[#3772ff1a] rounded-full  w-[88px] h-[88px] flex items-center justify-center">
+                  <Image
+                    priority
+                    width="100"
+                    height="100"
+                    src={icon}
+                    alt={`${header} icon`}
+                  />
+                </div>
+              </div>
+
+              <div>
+                <p className="mb-3 text-2xl font-bold">{header}</p>
+                <p className="text-[#7e7e7e]">{description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StatsWithImage;
